a3e78116ceac4cee2c791fe3d18ad2f94961fd3a
Martin Otero <martin_otero@hotmail.com>
2024-06-24 12:17:20 -0300

    3

---
+++ src/app/components/product.js
@@ -0,0 +1,8 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Product() {
+  return <div>Producto Aquí</div>;
+}
