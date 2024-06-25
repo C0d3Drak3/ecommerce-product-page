6930afa981381da233990756a2226b98f158608f
Martin Otero <martin_otero@hotmail.com>
2024-06-25 12:28:30 -0300

    4

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,22 +6,10 @@ import Link from "next/link";
 import logo from "../../../public/images/logo.svg";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isOpen2, setIsOpen2] = useState(false);
-
-  const toggle = () => {
-    setIsOpen((old) => !old);
-  };
-
-  const toggle2 = () => {
-    setIsOpen2((old2) => !old2);
-  };
-
-  const transClass = isOpen ? "flex" : "hidden";
-  const transClass2 = isOpen2 ? "flex" : "hidden";
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="  w-full h-[70px] flex flex-row items-center font-Epilogue ">
+    <div className="  w-full h-[70px] flex flex-row items-center font-Epilogue border-neutral-400 border-b-2 border-opacity-20 ">
       <div className="flex flex-row  m-4 ">
         <Image
           src={logo}
@@ -35,169 +23,50 @@ export default function Navbar() {
       {
         <div className="flex  w-full justify-between">
           {/* menu buttons */}
-          {/*
-        <div className="flex flex-row text-gray-500  font-medium">
-          <div className="m-5 relative">
+          <div className="flex flex-row text-gray-500  font-medium">
             <button
-              className="hover:text-black flex flex-row items-center"
-              onClick={toggle}
+              className="hover:text-black flex flex-row items-center border-orange-500 hover:border-b-2 mx-4"
+              onClick=""
             >
-              Features
-              {isOpen ? (
-                <Image
-                  src={arrowU}
-                  alt="arrowU"
-                  width={25}
-                  height={25}
-                  className=" mx-1 w-4 h-auto  "
-                />
-              ) : (
-                <Image
-                  src={arrowD}
-                  alt="arrowD"
-                  width={25}
-                  height={25}
-                  className=" mx-1 w-4 h-auto  "
-                />
-              )}
+              Collections
             </button>
-            <div
-              className={`absolute top-8 right-1 z-30 w-[150px] min-h-[170px] flex flex-col py-4 bg-zinc-100 shadow-md shadow-gray-800 rounded-md ${transClass}`}
-            >
-              <Link
-                className=" hover:text-black px-4 py-1 flex flex-row items-center"
-                href=""
-                onClick={toggle}
-              >
-                <Image
-                  src={todo}
-                  alt="todo"
-                  width={25}
-                  height={25}
-                  className=" mx-2 w-4 h-auto  "
-                />
-                Todo List
-              </Link>
-              <Link
-                className=" hover:text-black px-4 py-1 flex flex-row items-center"
-                href=""
-                onClick={toggle}
-              >
-                <Image
-                  src={calendar}
-                  alt="calendar"
-                  width={25}
-                  height={25}
-                  className=" mx-2 w-4 h-auto  "
-                />
-                Calendar
-              </Link>
-              <Link
-                className=" hover:text-black px-4 py-1 flex flex-row items-center"
-                href=""
-                onClick={toggle}
-              >
-                <Image
-                  src={reminders}
-                  alt="reminders"
-                  width={25}
-                  height={25}
-                  className=" mx-2 w-4 h-auto  "
-                />
-                Reminders
-              </Link>
-              <Link
-                className=" hover:text-black px-4 py-1 flex flex-row items-center"
-                href=""
-                onClick={toggle}
-              >
-                <Image
-                  src={planning}
-                  alt="planning"
-                  width={25}
-                  height={25}
-                  className=" mx-2 w-4 h-auto  "
-                />
-                Planning
-              </Link>
-            </div>
-          </div>
-          <div className="m-5 relative">
-            <button
-              className="hover:text-black flex flex-row items-center"
-              onClick={toggle2}
-            >
-              Company{" "}
-              {isOpen2 ? (
-                <Image
-                  src={arrowU}
-                  alt="arrowU"
-                  width={25}
-                  height={25}
-                  className=" mx-1 w-4 h-auto  "
-                />
-              ) : (
-                <Image
-                  src={arrowD}
-                  alt="arrowD"
-                  width={25}
-                  height={25}
-                  className=" mx-1 w-4 h-auto  "
-                />
-              )}
-            </button>
-            <div
-              className={`absolute top-8 z-30 w-[150px] min-h-[130px] flex flex-col py-4 bg-zinc-100 shadow-md shadow-gray-800  rounded-md ${transClass2}`}
-            >
-              <Link
-                className="hover:text-black px-4 py-1"
-                href=""
-                onClick={toggle2}
-              >
-                History
-              </Link>
-              <Link
-                className="hover:text-black px-4 py-1"
-                href=""
-                onClick={toggle2}
-              >
-                Our Team
-              </Link>
-              <Link
-                className="hover:text-black px-4 py-1"
-                href=""
-                onClick={toggle2}
-              >
-                Blog
-              </Link>
-            </div>
-          </div>
 
-          <div className="m-5 relative">
             <button
-              className="hover:text-black flex flex-row items-center"
+              className="hover:text-black flex flex-row items-center border-orange-500 hover:border-b-2 mx-4"
               onClick=""
             >
-              Careers{" "}
+              Men
             </button>
-          </div>
-          <div className="m-5 relative">
+
             <button
-              className="hover:text-black flex flex-row items-center"
+              className="hover:text-black flex flex-row items-center border-orange-500 hover:border-b-2 mx-4"
               onClick=""
             >
-              About{" "}
+              Women
+            </button>
+
+            <button
+              className="hover:text-black flex flex-row items-center border-orange-500 hover:border-b-2 mx-4"
+              onClick=""
+            >
+              About
+            </button>
+
+            <button
+              className="hover:text-black flex flex-row items-center border-orange-500 hover:border-b-2 mx-4"
+              onClick=""
+            >
+              Contact
             </button>
           </div>
-        </div>*/}
 
           {/* Login buttons */}
           <div className="flex flex-row  h-[60px] mr-4  ">
             <button className=" rounded-2xl w-[100px] h-[45px]  place-self-center font-Epilogue  text-gray-500 font-semibold  hover:text-black ">
-              Login
+              Cart
             </button>
             <button className=" rounded-2xl w-[100px] h-[45px]  place-self-center font-Epilogue border-2 border-gray-500 hover:border-black text-gray-500 font-semibold  hover:text-black ">
-              Register
+              User
             </button>
           </div>
         </div>
--- src/app/components/product.js
+++ src/app/components/product.js
@@ -3,6 +3,15 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import Zapas from "../../../public/images/image-product-1.jpg";
+
 export default function Product() {
-  return <div>Producto Aquí</div>;
+  return (
+    <div className="flex flex-row justify-between text-black">
+      <div className="flex border-2 border-black h-[600px] w-[400px] justify-center p-12">
+        <Image src={Zapas} alt="zapatillas"></Image>
+      </div>
+      <div className="flex border-2 border-black h-[600px] w-[400px] justify-center"></div>
+    </div>
+  );
 }
